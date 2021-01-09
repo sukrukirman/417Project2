037be60056f59942d144208e4865495b7ac18e2b
mehmet <ali.karakamis@ug.bilkent.edu.tr>
2021-01-09 23:10:40 +0300

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+	public static int drawMenu() {
+		Scanner scanner = new Scanner(System.in);
+		int selection;
+		do {
+			System.out.println("1) Give order");
+			System.out.println("2) See my order");
+			System.out.println("3) Exit");
+			System.out.println("Status: ON");
+			System.out.print("Enter your selection: ");
+			selection = scanner.nextInt();
+		} while(selection < 1 || selection > 3);
+		return selection;
+	}
 
-    }
-
+	public static void main(String[] args) {
+		int selection = drawMenu();
+	}
 }
